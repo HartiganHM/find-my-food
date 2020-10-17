bf7c3d32bbf8ffff3dde0c4e4bee0fda00abc98f
Hugh Hartigan <Hartigan.HM@gmail.com>
2020-10-16 18:55:17 -0600

    Updates

---
+++ precache-manifest.534bd4d525320bdd55f9198479bf2ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bad7a3b274d89447784a3be0438f61",
+    "url": "/find-my-food/index.html"
+  },
+  {
+    "revision": "c0dee6b4b03961d4dda9",
+    "url": "/find-my-food/static/css/main.e18be82e.chunk.css"
+  },
+  {
+    "revision": "7e389553ce91a371a2e9",
+    "url": "/find-my-food/static/js/2.e01211a0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/find-my-food/static/js/2.e01211a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0dee6b4b03961d4dda9",
+    "url": "/find-my-food/static/js/main.2dd0c122.chunk.js"
+  },
+  {
+    "revision": "a7eb304ce847226f4c4c",
+    "url": "/find-my-food/static/js/runtime-main.a49def67.js"
+  }
+]);
